4ee5e0216a439b5d3411a950d89f261764021abf
Anton Linevich <anton@linevich.net>
2016-12-01 17:24:18 +0200

    Added dummy example model

--- example/sample/models.py
+++ example/sample/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class DummyModel(models.Model):
+    name = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True
+    )
+    description = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return str(self.name)
